d7bfea2db8e1ecfc99f92c97e2e65414f169d2fb
Izan <87086679+sarry20@users.noreply.github.com>
2026-03-27 09:49:09 +0100

    fix: forgot mapping

--- src/main/java/com/ccsw/tutorial/author/AuthorController.java
+++ src/main/java/com/ccsw/tutorial/author/AuthorController.java
@@ -40,7 +40,9 @@ public class AuthorController {
 
         return new PageImpl<>(page.getContent().stream().map(e -> mapper.map(e, AuthorDto.class)).collect(Collectors.toList()), page.getPageable(), page.getTotalElements());
     }
-
+    
+    @Operation(summary = "Find", description = "Method that return a list of Authors")
+    @RequestMapping(path = "", method = RequestMethod.GET)
     public List<AuthorDto> findAll() {
 
         List<Author> authors = this.authorService.findAll();
