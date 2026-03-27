00637d306f6d4358d900ed83d0edc219fc3df080
Izan <87086679+sarry20@users.noreply.github.com>
2026-03-27 11:43:50 +0100

    fix: now client cant be duplicated

--- src/main/java/com/ccsw/tutorial/client/model/Client.java
+++ src/main/java/com/ccsw/tutorial/client/model/Client.java
@@ -11,7 +11,7 @@ public class Client {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     /**
